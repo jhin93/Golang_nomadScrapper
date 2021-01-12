df1ab71fa8789402ca788275080c0f3e07ad1a27
jhin93 <scene1993@gmail.com>
2021-01-13 00:47:50 +0900

    함수의 시작이 대문자면 export가 가능하고 아니라면 불가하다(private)

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/jhin93/learngo/something"
 )
 
 // 2. function main이 Go program의 시작점이 되는 부분.
 // 컴파일러는 main package와 main function을 먼저 찾고 실행시킨다.
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello world")
+	// SayHello는 something.go에서 함수가 대문자로 작성되었기에 export가 가능하다.
+	// 그렇기에 SayHello는 export되었고 7번째 줄에 import된 모습이다(vscode extension이 자동으로 추가해줌)
+	something.SayHello()
+	// 하지만 sayBye는 대문자로 시작하지 않기에 private한 함수이다.
+	something.sayBye()
+
 }
