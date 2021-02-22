809d7b8ac6bbe952a5c0ddb04478cce923b2acb0
jhin93 <scene1993@gmail.com>
2021-02-22 19:06:12 +0900

    index까지 출력.

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import "fmt"
 // Go에서 loop는 오로지 for를 사용하는 것으로만 가능하다.
 // range는 array에 loop를 적용할 수 있도록 해준다.
 func superAdd(numbers ...int) int {
-	for number := range numbers {
-		fmt.Println(number)
-	}
+	for a, b := range numbers {
+		fmt.Println(a, b)
+	} // a 에는 index가, b에는 값이 출력된다.
 	return 2 // return type이 int이라서 return 1 을 하는 것.
 }
 
 func main() {
 	superAdd(1, 2, 3, 4, 5, 6)
 }
+
+// 결과
+// 0 1
+// 1 2
+// 2 3
+// 3 4
+// 4 5
+// 5 6
