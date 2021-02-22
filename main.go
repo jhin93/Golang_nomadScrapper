b2a3376ae9865a4d6f3cb0b0ad64b629346513d6
jhin93 <scene1993@gmail.com>
2021-02-22 19:20:33 +0900

    for a, b := 는 인덱스, 내용물.

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import "fmt"
 
 // Go에서 loop는 오로지 for를 사용하는 것으로만 가능하다.
 // range는 array에 loop를 적용할 수 있도록 해준다.
+// 또한 range는 오로지 for 안에서만 적용할 수 있다.
 func superAdd(numbers ...int) int {
-	for a, b := range numbers {
-		fmt.Println(a, b)
-	} // a 에는 index가, b에는 값이 출력된다.
-	return 2 // return type이 int이라서 return 1 을 하는 것.
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func main() {
-	superAdd(1, 2, 3, 4, 5, 6)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
 
-// 결과
-// 0 1
-// 1 2
-// 2 3
-// 3 4
-// 4 5
-// 5 6
+// 결과 21
+
+// for a, b := 는 인덱스, 내용물.
+// 그래서 이 함수에선 total에 0부터가 아닌 1부터 더한다. 함수의 결과는 21.
