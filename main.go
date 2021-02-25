574196c33f3c961e6c969c36262056f5bac3deba
jhin93 <scene1993@gmail.com>
2021-02-25 23:13:04 +0900

    Low-Level programming

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import "fmt"
 
-// 자바스크립트의 switch와 비슷하다
-// if-else가 난무하는 상황을 피할 수 있다.
-// switch를 작성하고 나서 if 처럼 variable을 만들 수 있다.
+// Low-Level programming.
 
 func canIDrink(age int) bool {
 	switch koreanAge := age + 2; koreanAge {
