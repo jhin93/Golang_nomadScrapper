1572130a1b6df97251a8ba73e0fe6f089b3a9632
jhin93 <scene1993@gmail.com>
2021-03-01 16:23:10 +0900

    Low-level Programming. 값 복사.

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import "fmt"
 
-// 커밋 테스트 10
-
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
+// Low-level Programming
+// 메모리 주소를 보는 법, 무엇인가를 메모리 주소에 접근시키는 방법.
+// 코드를 작성하다 보면 복사가 되는 것을 원치 않을 수 있다.
+// 메모리까지 찾아가고 싶을 수 있음.
+// (값 복사의 예시 -> a := 2 // b := a)
 
 func main() {
-	fmt.Println(canIDrink(10))
+	a := 2
+	b := a
+	// 이 시점 이후로 a에 어떤 값이 들어가도 b에 영향이 가지 않는다. 이 시점의 b는 2.
+	a = 10
+	fmt.Println(a, b)
 }
