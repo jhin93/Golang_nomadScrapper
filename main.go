53aeadc75186250b61403a254f5eca7e8fc04b3f
jhin93 <scene1993@gmail.com>
2021-03-03 23:41:44 +0900

    map

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+// 데이터 구조 : map
+// 자바스크립트의 object와 비슷하다.
+
+func main() {
+	// 작성하는 법
+	// map[key의 타입]value의 타입{key:value}
+	// 아래 map의 key는 string, value도 string.
+	nico := map[string]string{"name": "nico", "age": "12"}
+	fmt.Println(nico)
 }
