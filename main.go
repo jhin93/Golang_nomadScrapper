9c3e92212adaca1045290802d55222f941a6aad9
jhin93 <scene1993@gmail.com>
2021-03-09 23:48:00 +0900

    struct는 field:value 혹은 value 둘중 하나만 사용해야 함.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// struct.
+// struct. go는 class도 객체도 없다.
 // struct는 자바스크립트의 object와 비슷하면서 map보다 유연한 것이 특징이다.
 // struct를 만들기 위해서는 어떤 struct인지 정의해주어야 한다.
 type person struct {
@@ -12,8 +12,8 @@ type person struct {
 }
 
 func main() {
-	// person에게 value를 주는 방법에는 두가지가 있다.
-	// 두번째 방법. 상단의 struct가 안보이거나 해도 어떤 식으로 이루어져있는지 알 수 있다.
+	// struct를 사용할 땐, field:value 혹은 value. 둘 중 하나로 통일해서 작성해야만 한다.
+	// 다음과 같이 field:value와 value를 섞어서 사용하면 안된다. nico := person{name: "nico", 18, favFood: favFood}
 	favFood := []string{"kimchi", "ramen"}
 	nico := person{name: "nico", age: 18, favFood: favFood}
 	//fmt.Println(nico) 혹은 fmt.Println(nico.age)
