be9003bc7be97dfbd1a0058d86c1b8920f60338e
jhin93 <scene1993@gmail.com>
2021-03-12 00:04:16 +0900

    2. export는 struct와 그 요소, 둘다 public(대문자)이어야 한다.

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/jhin93/learngo/banking"
+)
+
 // bankAccount
 
+// 'banking'이라는 package의 Account라는 struct를 사용.
+// Account라는 struct를 구조 그대로 사용해야 한다. Account, Owner, Balance 다 대문자로 사용할 것.
+
 func main() {
-	account := banking.
+	account := banking.Account{Owner: "nicolas", Balance: 1000}
+	fmt.Println(account)
 }
