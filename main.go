e77dbde34b171fea97706fc4ea8961d8973ca7be
jhin93 <scene1993@gmail.com>
2021-03-14 16:09:33 +0900

    ,를 많이 찍을 수록 많은 값 가능

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import "fmt"
 
 // 함수에서 리턴을 여러개 해보기. 타입이 서로 다른 리턴값을 여러개 만들 순 없을까?
 // https://fenderist.tistory.com/187
-func multiply() (int, string) {
+func multiply() (int, string, bool) {
 
-	return 1, "stir"
+	return 1, "stir", true
 }
 
 func main() {
-	a, b := multiply()
-	fmt.Println(a, b)
+	a, b, c := multiply()
+	fmt.Println(a, b, c)
 }
