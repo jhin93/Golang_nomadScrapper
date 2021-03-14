e8da0187dc40920b37a8abdea3273a484ba7da43
jhin93 <scene1993@gmail.com>
2021-03-14 16:08:24 +0900

    타입이 서로 다른 여러 리턴값 도출 가능. 리턴 값에 콤마(,) 가 빠졌었음

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
+import "fmt"
+
 // 2020.3.13 지금까지 한것에 대한 복습.
 
-// 함수에서 리턴을 여러개 해보기
+// 함수에서 리턴을 여러개 해보기. 타입이 서로 다른 리턴값을 여러개 만들 순 없을까?
 // https://fenderist.tistory.com/187
-func multiply() (int string) {
+func multiply() (int, string) {
 
+	return 1, "stir"
 }
 
 func main() {
-
+	a, b := multiply()
+	fmt.Println(a, b)
 }
