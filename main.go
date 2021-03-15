7a9cb9ad56d3eefc094c5542f9013d4619fbec7f
jhin93 <scene1993@gmail.com>
2021-03-16 00:00:02 +0900

    eee

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // 2020.3.13 지금까지 한것에 대한 복습.
 
-// 함수에서 리턴을 여러개 해보기. 타입이 서로 다른 리턴값을 여러개 만들 순 없을까?
+// 함수에서 리턴을 여러개 해보기. 
 // https://fenderist.tistory.com/187
 func multiply() (int, string, bool) {
 
