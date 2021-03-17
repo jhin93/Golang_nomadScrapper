0ef2dd2eb3250d69885ceb9a6420a3dba8a1404c
jhin93 <scene1993@gmail.com>
2021-03-17 23:06:09 +0900

    3/17 복습

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import "fmt"
 
 // 2020.3.13 지금까지 한것에 대한 복습.
-
-// 함수에서 리턴을 여러개 해보기. 
-// https://fenderist.tistory.com/187
-func multiply() (int, string, bool) {
-
-	return 1, "stir", true
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	a, b, c := multiply()
-	fmt.Println(a, b, c)
+
 }
