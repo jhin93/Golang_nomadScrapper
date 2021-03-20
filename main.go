d63da11a18cc3fd92d58153d5a71eb9eda368ed3
jhin93 <scene1993@gmail.com>
2021-03-20 10:24:52 +0900

    3/20 defer 주석 수정

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// 2020.3.13 지금까지 한것에 대한 복습.
-func repeatMe(words ...string) {
-	fmt.Println(words)
+// defer
+// defer는 function이 값을 return하고 나면 실행된다.
+// function이 실행되고 나서 코드가 실행된다.
+// 아래 코드는 다음과 같은 순서로 코드가 작동한다.
+// 1. lenAndUpper 함수가 실행된다. 그리고 return이 되지만 fmt.Println이 없어 티가 안남.
+// 2. lenAndUpper의 defer가 실행된다. lenAndUpper가 끝났으니까.
+// 3. func main의 fmt.Println가 실행된다.
+// 4. func main의 defer 가 실행된다. 이 defer가 속한 'main'함수 실행이 끝났으니까.
+
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	repeatMe("nico", "lynn", "dal", "flynn")
+	defer fmt.Println("I'm last")
+	totalLength, upper := lenAndUpper("nico")
+	fmt.Println(totalLength, upper)
 }
