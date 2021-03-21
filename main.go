eef65992d5a1aa3662182f2fcbbdaed51d204e52
jhin93 <scene1993@gmail.com>
2021-03-21 15:22:24 +0900

    for 문으로 반복해서 더한 값 구하기

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func superAdd(fff ...int) int {
-	for a, b := range fff {
-		fmt.Println(a, b)
+	sum := 0
+	for _, b := range fff {
+		sum += b
 	}
-	return 2 // 리턴값을 int로 설정했으니 아무 int나 넣어놓은 것.
+	return sum // 리턴값을 int로 설정했으니 아무 int나 넣어놓은 것.
 }
 
 func main() {
-	superAdd(10, 20, 30)
+	result := superAdd(10, 20, 30)
+	fmt.Println(result)
 }
 
 // 결과
-// 0 10
-// 1 20
-// 2 30
+// 60
