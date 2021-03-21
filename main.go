7a2eff12daa1133d5919026d64fbd9044103b376
jhin93 <scene1993@gmail.com>
2021-03-21 15:27:03 +0900

    for a는 인덱스. for _, b는 밸류.

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func superAdd(fff ...int) int {
 	sum := 0
-	for _, b := range fff {
-		sum += b
+	for a := range fff {
+		sum += a
 	}
 	return sum // 리턴값을 int로 설정했으니 아무 int나 넣어놓은 것.
 }
@@ -16,4 +16,6 @@ func main() {
 }
 
 // 결과
-// 60
+// 3
+// 인덱스를 전부 더한 결과이기에 3이 출력된다. (0+1+2)
+// 만약 밸류들을 더하고 싶다면 _, b와 같이 작성해서 인덱스를 무시하고 뒤에 따라오는 밸류만 사용한다.
