9ef5cde6528b467195556f088fa3bc0d17789b6a
jhin93 <scene1993@gmail.com>
2021-03-22 23:48:54 +0900

    if문에서 ()를 사용하지 않아도 된다.

--- main.go
+++ main.go
@@ -2,20 +2,13 @@ package main
 
 import "fmt"
 
-func superAdd(fff ...int) int {
-	sum := 0
-	for a := range fff {
-		sum += a
+func canIdrink(age int) bool {
+	if koreanAge := age + 1; koreanAge > 19 {
+		return true
 	}
-	return sum // 리턴값을 int로 설정했으니 아무 int나 넣어놓은 것.
+	return false
 }
 
 func main() {
-	result := superAdd(10, 20, 30)
-	fmt.Println(result)
+	fmt.Println(canIdrink(19))
 }
-
-// 결과
-// 3
-// 인덱스를 전부 더한 결과이기에 3이 출력된다. (0+1+2)
-// 만약 밸류들을 더하고 싶다면 _, b와 같이 작성해서 인덱스를 무시하고 뒤에 따라오는 밸류만 사용한다.
