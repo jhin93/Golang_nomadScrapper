c84e216c28f26a4294741fb855d577b5f74eebe5
jhin93 <scene1993@gmail.com>
2021-03-24 23:35:55 +0900

    switch 문.

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import "fmt"
 
 func canIdrink(age int) bool {
-	if koreanAge := age + 1; koreanAge > 19 {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
 		return true
 	}
 	return false
 }
 
 func main() {
-	fmt.Println(canIdrink(19))
+	fmt.Println(canIdrink(20))
 }
+
+// 20을 인자로 넣어도 false가 도출된다.
+// 10 혹은 18만 리턴되어야 하기 때문.
