57bf7f088aba2b1a222e9f578d9abbeaacb89047
jhin93 <scene1993@gmail.com>
2021-03-28 19:33:38 +0900

    Go에서 constructor를 만들어 사용하는 방법. 받는 인자를 잘 조작할 것.

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,21 +1,16 @@
 package accounts
 
 // Account struct
+// export를 하기 위해선 주석으로 export하는 대상을 언급해주어야 한다.
+// 대문자는 public, 소문자는 private.
 type Account struct {
 	owner   string
 	balance int
 }
 
-// struct도 private이 될 수 있고(Account -> account),
-// public인 function을 만들 수 있다. 이 function이 struct를 만들 것.
-
-// NewAccount creates Account. NewAccount는 owner만을 갖는다. 그리고 account의 복사본을 리턴한다. *를 붙여서. *는 살펴보는 기능이다.
+// NewAccount is account-making function.
+// 여기서 NewAccount의 인자를 owner만 받고 balance는 0으로 고정해버리면, 사용자는 새 계좌를 만들 때 임의로 balance를 조작할 수 없다.
 func NewAccount(owner string) *Account {
-	// 1. account를 초기화한다.
 	account := Account{owner: owner, balance: 0}
-	// 2. 복사본의 address(&)를 return한다. 새로운 object를 리턴하고 싶은데 만들긴 싫으니 메모리 주소를 return. 항상 값 복사를 하면 데이터가 늘어나 느려진다.
 	return &account
-	// 3. &account = account의 메모리. 'acount의 메모리'의 '값'을 *을 붙인 Account를 리턴함으로써 알 수 있다.
-	// *를 사용하면 메모리 주소의 값 만을 살펴보는 것이라서, 메모리 주소에 새로운 값이 대입되도 그것을 확인할 수 있다.
-	// 추가로,단순히 값 복사를 하면 복사하기 바로 전 시점의 메모리 값을 복사하기에 그 이후에 새로 메모리 값이 변해도 그것을 알 수 없다.
 }
--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// struct. 구조체. ※ Go에는 클래스도, 객체도 없다.
-// struct는 자바스크립트의 object와 비슷하면서 map보다 유연한 것이 특징이다.
-// 유연한 이유는 밸류의 타입을 map보다 자유롭게 사용 가능하기 때문일 듯. map은 키와 밸류를 각각 지정하면 그대로 사용해야 함.
-// struct를 만들기 위해서는 어떤 struct인지 정의해주어야 한다.
+	"github.com/jhin93/learngo/accounts"
+)
 
+// GO에서 constructor를 만드는 방법
+
+// 1. function(ex NewAccount)을 만들어서 object를 return한다. 복사본이 아닌 실제 메모리 address를 return. 항상 복사본을 만드는 것은 지양.
+//
 func main() {
-	type person struct {
-		name    string
-		age     int
-		favFood []string
-	}
-	food := []string{"kimchi", "ramen"}
-	nico := person{name: "nico", age: 18, favFood: food}
-	fmt.Println(nico.favFood)
-	// 결과 [kimchi ramen]
-
-	// struct를 작성할 땐 field:value 혹은 value. 둘중 하나만 사용해야 한다. 다음과 같이 섞는 것은 금물
-	nico := person{name: "nico", 18, favFood: favFood}
-
-	// field:value 형식으로 작성하는 것을 추천. value만 작성하면 상단의 구조체를 보면서 field를 확인해야 한다.
-
+	account := accounts.NewAccount("nico")
+	fmt.Println(account)
+	// 결과
+	// &{nico 278}
+	account.balance = 1000
+	// 위의 문장은 불가하다. 왜냐하면 import되는 NewAccount에서는 오직 owner만 인자로 받기 때문.
 }
