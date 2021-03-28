3ec8340edc34f1781ff7060bd1a355ba49529229
jhin93 <scene1993@gmail.com>
2021-03-28 17:43:03 +0900

    Go에서의 Array는 길이와 타입을 명시해야 한다. + 코드 실행순서는 위에서부터

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
-// 포인터(*)를 이용하여 메모리 주소의 값을 변경할 수 있다.
-// a의 메모리 주소에 새로운 값을 대입했고, 이는 곧 a에 대입한 것으로 나타난다.
+// Arrays and Slices
+// Go에서는 Array를 만들 때 길이와 타입을 명시해주어야 한다.
+
 func main() {
-	a := 2
-	b := &a
-	*b = 20
-	fmt.Println(a, *b)
-	// 결과 20 20
+	names := [5]string{"nico", "lynn", "dal"}
+	fmt.Println(names)
+	// 결과 [nico lynn dal  ]
+	// 이 시점(fmt.Println(names)) 이후로 names에 추가로 값을 대입했다면
+	// 새롭게 fmt.Println(names)를 실행해 줘야 추가로 대입한 값을 확인할 수 있다.
+	fmt.Println(names[3])
+	// 결과 (공백)
+	// 오류가 나진 않고 인덱스에 밸류가 빈 상태로 출력됨.
+	names[3] = "lalala"
+	fmt.Println(names)
+	// 결과 [nico lynn dal lalala ]
+	// 처음 실행한 fmt.Println(names)에서는 'lalala'를 확인할 수 없다. 이후에 추가로 대입한 값이기 때문.
 }
