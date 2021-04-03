f723153a7fd00cc33bb4bd3f4f4a1f423dbb8662
jhin93 <scene1993@gmail.com>
2021-04-03 16:34:00 +0900

    receiver 작성하기. (Go에서의 메소드)

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "fmt"
+
 // Account struct
 // export를 하기 위해선 주석으로 export하는 대상을 언급해주어야 한다.
 // 대문자는 public, 소문자는 private.
@@ -12,4 +14,26 @@ type Account struct {
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// 메소드 작성하기.
+// func와 함수명 사이에 receiver를 적어준다.
+// Go에서는 receiver가 메소드와 동등하다.
+
+// receiver를 작성 규칙.
+// 1. function의 이름 전에 작성한다.
+// 2. struct의 첫 글자를 따서 소문자로 지어야 한다.
+// ex) func (a Account) Deposit. --> receiver의 이름은 a이고 a의 타입은 Account라는 struct이다.
+
+// Deposit x amount on your account
+// 이 Deposit은 리턴할 값을 쓰지 않았다.
+func (a Account) Deposit(amount int) {
+	// 메서드 내에서 a로 접근을 하면 owner, balance를 변경할 수 있다.
+	fmt.Println("Gonna deposit", amount)
+	a.balance += amount
+}
+
+// Balance of your account. account의 balance만 보이도록 method를 작성.
+func (a Account) Balance() int {
+	return a.balance
 }
--- main.go
+++ main.go
@@ -8,4 +8,7 @@ import (
 
 func main() {
 	account := accounts.NewAccount("nico")
+	account.Deposit(10)
+	// account의 balance만 보이는 func Balance를 적용.
+	fmt.Println(account.Balance())
 }
