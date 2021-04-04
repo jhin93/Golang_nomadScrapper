958d9312f67f3e54d3bf946ed8264a606716e690
jhin93 <scene1993@gmail.com>
2021-04-04 18:56:20 +0900

    2. 에러를 체크하는 방법. 항상 에러를 직접 체크해야 한다.

--- main.go
+++ main.go
@@ -11,14 +11,17 @@ func main() {
 	account.Deposit(10)
 	// account의 balance만 보이는 func Balance를 적용.
 	fmt.Println(account.Balance())
-	account.Withdraw(20)
+	// account.Withdraw()로부터 error를 받아온다.
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(account.Balance())
 }
 
-// 결과는 다음과 같다.
+// 실행결과
 // 10
+// Can't withdraw you are pooor
 // 10
-// 아무 변화가 없다.
-// 왜냐하면, Withdraw에서 error를 return 했다는 이유로 Go가 관여를 하지 않기 때문. 그렇기에 balance는 여전히 10으로 출력됨.
-// 그러나 Go는 자바스크립트의 try catch와 같은 것이 없기에, 개발자가 에러를 알 수 없다.
-// 그렇기에 error를 다룰 코드를 따로 작성해주어야만 한다.
+
+// err := .... -> 앞으로 흔하게 쓰게 될 것. 항상 error를 체크해야 한다.
