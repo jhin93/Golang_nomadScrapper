f88f7db12a498787085aef896955c7f0ce9d29ae
jhin93 <scene1993@gmail.com>
2023-06-10 18:18:12 +0900

    컴파일 하려면 package main, main.go

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jhin93/learngo/accounts"
-)
+import "fmt"
 
 func main() {
-	account := accounts.NewAccount("nico")
-	account.Deposit(10)
-	// account의 balance만 보이는 func Balance를 적용.
-	fmt.Println(account.Balance())
-	// account.Withdraw()로부터 error를 받아온다.
-	err := account.Withdraw(20)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println("Hello world!")
 }
