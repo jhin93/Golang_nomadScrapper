6282fbc14764882e3c75742e361dbe473bade791
jhin93 <scene1993@gmail.com>
2023-06-10 19:06:28 +0900

    축약형은 func 안에서만, var 에만 적용된다.

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/jhin93/learngo/something"
-)
-
+// name := "nico" // 함수 밖에서는 사용할 수 없다.
 func main() {
-	fmt.Println("Hello world!") // Go에서는 모듈을 export 하기 위해선 해당 모듈이 대문자로 시작해야 한다. 그래서 Println이 대문자 P로 시작하는 것.
-	something.SayHello()        // somethig.go의 SayHello는 대문자로 작성되었기에 export 됨.
-	// something.sayBye() // sayBye는 private 함수. 함수가 대문자로 시작하지 않기 때문에 export 되지 않아 사용할 수 없다.
+	// 축약형은 오직 func 안에서만, 그리고 변수(var)에만 적용 가능하다. 축약형이 존재하면 Go가 첫번째 값을 기준으로 변수의 type을 찾아 정해준다.
+	name := "nico" // := 는 type입력을 생략하게 해준다. 정해진 type은 임의로 변경할 수 없다.
+	name = "hjie"
+	fmt.Println(name)
 }
