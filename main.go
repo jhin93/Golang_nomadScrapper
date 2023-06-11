c6fb93a62145d8e4123f90cf09936ba554dfb1c8
jhin93 <scene1993@gmail.com>
2023-06-11 17:45:09 +0900

    'defer' 는 함수가 종료된 이후 실행된다.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func lenAndUpper(name string) (length int, uppercase string) {
+	// defer는 함수 실행이 종료되고 나서 진행되는 로직임.
+	defer fmt.Println("I'm done")
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
@@ -16,4 +18,6 @@ func main() {
 	fmt.Println(totalLenth, uppercase)
 }
 
-// result : 4 NICO
+// result
+// I'm done - 함수 lenAndUpper 가 실행되고 난 이후 defer에 의해 출력됨
+// 4 NICO - 함수 main에 의해 출력
