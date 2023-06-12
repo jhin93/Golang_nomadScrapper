241a726b8aa8e8d01837dd15fb6aa1ea6b4d4a92
jhin93 <scene1993@gmail.com>
2023-06-12 21:11:46 +0900

    조건문 작성 예시(+ 내부변수 생성)

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import "fmt"
 
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 { // 이런식으로 조건문 안에 변수를 만드는 게 오직 조건문에서만 사용되는 변수라는 걸 알려준다.
+		return false
 	}
-	return total
+	return true
 }
 
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
