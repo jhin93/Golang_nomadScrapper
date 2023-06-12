8138d12d5ce8ebcd1bdc5643b9ecb116dbac0cda
jhin93 <scene1993@gmail.com>
2023-06-12 21:14:34 +0900

    switch 예제

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 { // 이런식으로 조건문 안에 변수를 만드는 게 오직 조건문에서만 사용되는 변수라는 걸 알려준다.
+	switch age {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	return false
 }
 
 func main() {
-	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink(18))
 }
