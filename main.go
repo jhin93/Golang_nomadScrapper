b985ec5233683052bda380540e0da17e87f8d4a4
jhin93 <scene1993@gmail.com>
2023-06-12 21:58:23 +0900

    slice 사용 예제

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	a = 5
-	fmt.Println(a, *b)
+	//slice는 길이 제한 없는 array
+	names := []string{"a", "b", "c"}
+	// 요소 추가를 위해선 append 함수 사용
+	names = append(names, "ee") // 새로운 slice 를 return하는 것
+	fmt.Println(names)
+	// [a b c ee]
 }
