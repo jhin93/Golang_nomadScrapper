bb8f0ecc0714d70e9b19d7360e3ff051410a2be4
jhin93 <scene1993@gmail.com>
2023-06-13 20:59:05 +0900

    struct 기본 형태

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	for key, _ := range nico {
-		fmt.Println(key)
-	}
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{"nico", 12, favFood}
+	fmt.Println(nico)
 }
