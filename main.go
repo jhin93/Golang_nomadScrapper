ba81d125996b621018d170c8a17a6c9aa7f8dcc2
jhin93 <scene1993@gmail.com>
2023-06-13 20:50:00 +0900

    key만 출력

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	nico := map[string]string{"name": "nico", "age": "12"}
-	for _, value := range nico {
-		fmt.Println(value)
+	for key, _ := range nico {
+		fmt.Println(key)
 	}
 }
