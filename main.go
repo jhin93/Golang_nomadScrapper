9a27b52341f8213b49e4d26b301cab62213b0948
jhin93 <scene1993@gmail.com>
2023-06-13 20:42:45 +0900

    map 기본형태 map[key]value

--- main.go
+++ main.go
@@ -3,10 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	//slice는 길이 제한 없는 array
-	names := []string{"a", "b", "c"}
-	// 요소 추가를 위해선 append 함수 사용
-	names = append(names, "ee") // 새로운 slice 를 return하는 것
-	fmt.Println(names)
-	// [a b c ee]
+	nico := map[string]string{"name": "nico", "age": "12"}
+	fmt.Println(nico)
 }
