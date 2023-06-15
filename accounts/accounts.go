9401ac87e4a9d2e01c0d0125b0bf2b83f469c90e
jhin93 <scene1993@gmail.com>
2023-06-15 20:42:44 +0900

    *Account에 대한 이해를 주석으로 기록

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -8,6 +8,9 @@ type Account struct {
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
+	// 주소를 저장하는 타입을 'pointer 타입'이라고 한다. 'pointer 타입' 은 '*'과 주소를 가진 데이터의 형태가 결합된 형태다(ex var ptr *int).
+	// 이 함수의 리턴 타입인 *Account는 '구조체 Account 형태의 데이터의 주소'를 의미한다.
+	// 그렇기 때문에 변수 account(구조체 Account 형태의 데이터)의 주소, 즉 &account가 리턴값이 되어야 한다.
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
