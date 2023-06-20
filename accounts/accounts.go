451eed925053a1e4b13c96f989c5110c95d6f326
jhin93 <scene1993@gmail.com>
2023-06-20 21:40:01 +0900

    main.go에서 Deposit(10)이 되지 않은 이유

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -20,6 +20,9 @@ func NewAccount(owner string) *Account {
 func (a Account) Deposit(amount int) {
 	fmt.Println("Gonna deposit", amount)
 	a.balance += amount
+	// main.go에서 Deposit(10)이 되지 않은 이유 :
+	// Deposit 메소드의 a가 main.go의 변수 account의 복사본이기 때문.
+	// 그래서 main.go의 account에는 10이 적립되지 않고, accounts.go의 a(main.go의 account 복사본)에 적립된 것.
 }
 
 // Balance of your account
