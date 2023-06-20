f33a547626a8c872f809fd7d63cc0c4a5135a4fb
jhin93 <scene1993@gmail.com>
2023-06-20 21:44:25 +0900

    Deposit 되도록 수정

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -17,12 +17,9 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) { // 포인터를 적용함으로써 Deposit 메소드를 호출한 것(main.go의 account)을 사용하라는 것. 복사본이 아니고.
 	fmt.Println("Gonna deposit", amount)
 	a.balance += amount
-	// main.go에서 Deposit(10)이 되지 않은 이유 :
-	// Deposit 메소드의 a가 main.go의 변수 account의 복사본이기 때문.
-	// 그래서 main.go의 account에는 10이 적립되지 않고, accounts.go의 a(main.go의 account 복사본)에 적립된 것.
 }
 
 // Balance of your account
