72b48f2b21a8aa4484bb4a0c00a63407c80100e6
jhin93 <scene1993@gmail.com>
2023-06-25 21:06:39 +0900

    account owner 출력

--- main.go
+++ main.go
@@ -9,10 +9,9 @@ import (
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
 	err := account.Withdraw(20)
 	if err != nil {
 		fmt.Println(err) // Fatalln은 Println을 호출하고 프로그램을 종료시키는 역할
 	} // 결과 : Can't withdraw you are poor
-	fmt.Println(account.Balance())
+	fmt.Println(account.Balance(), account.Owner())
 }
