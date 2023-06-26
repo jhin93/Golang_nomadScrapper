02f1c1d0275d21780864e405ade9279eaf3b9155
jhin93 <scene1993@gmail.com>
2023-06-26 23:00:29 +0900

    type에 대한 메소드 예제

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/jhin93/learngo/mydict"
+import (
+	"fmt"
+
+	"github.com/jhin93/learngo/mydict"
+)
 
 func main() {
-	dictionary := mydict.Dictionary{}
+	dictionary := mydict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -1,4 +1,19 @@
 package mydict
 
+import "errors"
+
 // Dictionary type
 type Dictionary map[string]string
+
+var errNotFound = errors.New("Not Found")
+
+// Search for a word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word] // value는 찾고자 하는 값(d[word])이고 exists는 boolean이다.
+	if exists {
+		// 찾는 value가 있다면, error 값은 nil로 반환한다.
+		//리턴 형태는 (string, error) 이고, 찾는 값이 존재해서 에러가 없으니까 nil로 반환하는 것.
+		return value, nil
+	}
+	return "", errNotFound // 찾는 값이 없으니까 string은 "", 에러는 미리 만들어둔 에러변수를 반환한다.
+}
