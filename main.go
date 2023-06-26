04d03974cd570b8883e8af58a280419b563a7deb
jhin93 <scene1993@gmail.com>
2023-06-26 22:42:44 +0900

    나머지 삭제

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jhin93/learngo/accounts"
-)
-
 func main() {
-	account := accounts.NewAccount("nico")
-	account.Deposit(10)
-	fmt.Println(account)
+
 }
