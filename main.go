fcf213a7757216a73ae0da3dedd1753519cc7fd2
jhin93 <scene1993@gmail.com>
2023-07-03 22:42:20 +0900

    Goroutine이 무효한 경우

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
 	// Goroutines이란 기본적으로 다른 함수와 동시에 실행시키는 함수
-	go sexyCount("nico") // go 를 붙임으로써 sexyCount("nico")를 마치고 sexyCount("flynn")가 실행되던 것에서 두 함수 동시 실행으로 바뀌었다.
-	sexyCount("flynn")
+	// 아래처럼 go가 둘다 붙어있으면, 첫번째 Goroutines과 두번째 Goroutines이 실행되고 끝나버린다. 더 진행할 작업이 없기 때문이다.
+	// Goroutines은 프로그램이 실행되는 동안, 즉 메인함수가 실행되는 동안만 유효하다.
+	go sexyCount("nico")
+	go sexyCount("flynn")
 }
 
 func sexyCount(person string) {
