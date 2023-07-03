fa6c09ee9f1b608737c1ce3eb0c6c6623905411d
jhin93 <scene1993@gmail.com>
2023-07-03 21:34:35 +0900

    GO를 쓰는 이유 + fmt.Println(err)

--- main.go
+++ main.go
@@ -35,9 +35,12 @@ func main() {
 }
 
 func hitURL(url string) error {
+	// 아래 로직은 하나씩 url을 체크하고 있다. 이는 자바스크립트나 파이썬도 가능하다. 굳이 GO를 쓸 이유가 없는 것.
+	// 그렇기에 위 url 들을 하나씩이 아니라 병렬적으로 동시에 체크하는 로직을 GO로 작성할 것.
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
