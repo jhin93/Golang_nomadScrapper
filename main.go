9c8a5b213715a9441035599de38b41afeac3b790
jhin93 <scene1993@gmail.com>
2023-07-03 22:38:10 +0900

    함수 동시실행 명령어 'go'

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	// Goroutines이란 기본적으로 다른 함수와 동시에 실행시키는 함수
+	go sexyCount("nico") // go 를 붙임으로써 sexyCount("nico")를 마치고 sexyCount("flynn")가 실행되던 것에서 두 함수 동시 실행으로 바뀌었다.
+	sexyCount("flynn")
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
 }
