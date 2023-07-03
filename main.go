2d2df38925fb111d8979eaeecefc470a5b4dc543
jhin93 <scene1993@gmail.com>
2023-07-03 22:43:21 +0900

    메인함수가 끝나버리면 goroutines도 소멸된다.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	// Goroutines이란 기본적으로 다른 함수와 동시에 실행시키는 함수
 	// 아래처럼 go가 둘다 붙어있으면, 첫번째 Goroutines과 두번째 Goroutines이 실행되고 끝나버린다. 더 진행할 작업이 없기 때문이다.
 	// Goroutines은 프로그램이 실행되는 동안, 즉 메인함수가 실행되는 동안만 유효하다.
+	// 메인함수가 끝나버리면 goroutines도 소멸된다.
 	go sexyCount("nico")
 	go sexyCount("flynn")
 }
