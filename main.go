77a2e36ddce9d915828ace3969ec81aa32150529
jhin93 <scene1993@gmail.com>
2023-07-06 22:24:21 +0900

    가장 늦은 메시지를 받으면, 프로그램이 종료되는 것

--- main.go
+++ main.go
@@ -32,11 +32,12 @@ func main() {
 
 	for i := 0; i < len(urls); i++ {
 		fmt.Println(<-c) // goroutine으로 부터 받은 메시지를 출력
+		// 이 반복문이 끝나는 시간은, url 체크하는데 가장 오래걸리는 사이트 체크하는 시간과 동일. 병렬적으로 실행되는 것.
+		// 가장 늦은 메세지를 받으면 프로그램이 완료되는 것.
 	}
 }
 
 func hitURL(url string, c chan<- result) { // <-c 는 channel로부터 받는다는 뜻인데, chan<-는 channel이 받는 역할만 한다는 뜻이다. 즉 <-c 채널로부터 받는 것은 못하는 것.
-	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
