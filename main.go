0d5f06e8235a2f1988482116c59b6ace609ef8fb
jhin93 <scene1993@gmail.com>
2023-07-06 21:59:51 +0900

    url 체크 코드 복원

--- main.go
+++ main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type result struct {
+	url    string
+	status string
+}
+
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	c := make(chan string)
-	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
-	for _, person := range people {
-		go isSexy(person, c)
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println("waiting for", i)
-		fmt.Println(<-c) // <-c 는 blocking operation. 이것 때문에 위의 waiting for가 0에서 멈춤.
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range urls {
+		fmt.Println(url, result)
 	}
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 3)
-	c <- person + " is sexy"
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
