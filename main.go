7a54dfd54a84052f2a78962be53ed2ad795357b3
jhin93 <scene1993@gmail.com>
2023-07-06 22:29:15 +0900

    반복문이 끝나는 시간은, url 체크하는데 가장 오래걸리는 사이트 체크하는 시간과 동일

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 )
 
-type result struct {
+type requestResult struct {
 	url    string
 	status string
 }
@@ -14,7 +13,8 @@ type result struct {
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	c := make(chan result)
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -31,18 +31,20 @@ func main() {
 	}
 
 	for i := 0; i < len(urls); i++ {
-		fmt.Println(<-c) // goroutine으로 부터 받은 메시지를 출력
+		result := <-c
+		results[result.url] = result.status
+		// goroutine으로 부터 받은 메시지를 출력
 		// 이 반복문이 끝나는 시간은, url 체크하는데 가장 오래걸리는 사이트 체크하는 시간과 동일. 병렬적으로 실행되는 것.
 		// 가장 늦은 메세지를 받으면 프로그램이 완료되는 것.
 	}
 }
 
-func hitURL(url string, c chan<- result) { // <-c 는 channel로부터 받는다는 뜻인데, chan<-는 channel이 받는 역할만 한다는 뜻이다. 즉 <-c 채널로부터 받는 것은 못하는 것.
+func hitURL(url string, c chan<- requestResult) { // c(변수 이름) chan<-(메시지 받기만 하는 채널) result(메시지 형태)
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		status = "FAILED"
 	}
 	// 마지막엔 result를 우리 채널로 보낸다.
-	c <- result{url: url, status: status}
+	c <- requestResult{url: url, status: status}
 }
