558773934d0d6d6226be79ebef6f02f221460d84
jhin93 <scene1993@gmail.com>
2023-07-10 17:02:25 +0900

    2023/07/10 17:00:15 Request failed with Status: 403 exit status 1 - url 바꾸면 됨

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
+// var baseURL string = "https://www.airbnb.com/"
 
 func main() {
 	// 1. 페이지들을 받아오기
 	// 2. 각각의 페이지 방문하기
 	// 3. 페이지로부터 job을 추출하기
 	// 4. 추출한 job을 엑셀에 집어넣기
-	pages := getPages()
+	getPages()
 
 }
 
@@ -21,6 +26,17 @@ func getPages() int {
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
+
+	// 이 getPages 함수가 끝났을 때 아래 goquery 문서를 닫아야 함.
+	// 왜냐하면 goquery의 res.Body는 기본적으로 IO(input & output)이기 때문.
+	// 이렇게 메모리가 새어나가는 걸 막을 수 있음.
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+
 	return 0
 }
 
